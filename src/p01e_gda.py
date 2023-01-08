583a5f4b99811392806f05b906aa16d289947ff8
MihirSutariya <sutariya.mihirkumar@iitgn.ac.in>
2023-01-08 20:04:27 +0530

    implemented fit

--- src/p01e_gda.py
+++ src/p01e_gda.py
@@ -27,6 +27,49 @@ class GDA(LinearModel):
         > clf.fit(x_train, y_train)
         > clf.predict(x_eval)
     """
+    def __init__(self,varia=None,musal =None, *args, **kwargs):
+        self.varia= varia
+        self.musal = musal
+        self.musal = kwargs.pop('mus')
+        
+        super(GDA, self).__init__(*args, **kwargs)
+    
+    def theta(self,y):
+        ans =0
+        for i in y:
+            if(y==1):
+                ans+=1
+        self.theta = ans/np.size(y)
+    
+    def mu(self,x,y) :
+        self.musal = []
+        for k in range(np.size(x)[1]):        
+            ans  = np.zeros((np.size(x)[1],1))
+            divi = 0;
+            for i in y:
+                if int(i)==k:
+                    divi+=1
+                    
+            for i in range(np.size(x)[0]):
+                temp = np.array([x[i]]).T
+                if(int(y[i])==k):
+                    ans+=temp
+                    
+            self.musal.append(ans/divi)
+    
+    def sigma(self,x,y):
+        m = np.size(y)
+        ans = np.zeros((np.size(x)[1],np.size(x)[1]))            
+        for i in range(m):
+            mus = self.musal[int(y[i])]
+            ans += np.dot(np.array([x[i]]).T-mus,np.array([x[i]]- mus.T))
+            
+        self.sigma = ans/m
+            
+                
+            
+                
+        
 
     def fit(self, x, y):
         """Fit a GDA model to training set given by x and y.
@@ -39,6 +82,10 @@ class GDA(LinearModel):
             theta: GDA model parameters.
         """
         # *** START CODE HERE ***
+        self.theta()
+        self.mu()
+        self.sigma()
+        
         # *** END CODE HERE ***
 
     def predict(self, x):
