438c18a8b5b0d6b3c089d12ed314e7ad411b9ca4
MihirSutariya <mihirsutaria007@gmail.com>
2023-01-13 00:38:43 +0530

    all code added don't know correct or wrong

--- src/p03d_poisson.py
+++ src/p03d_poisson.py
@@ -1,6 +1,6 @@
 import numpy as np
 import util
-
+from scipy.stats import poisson
 from linear_model import LinearModel
 
 
@@ -14,7 +14,12 @@ def main(lr, train_path, eval_path, pred_path):
         pred_path: Path to save predictions.
     """
     # Load training set
-    x_train, y_train = util.load_dataset(train_path, add_intercept=False)
+    x_train, y_train = util.load_dataset("../data/ds4_train.csv", add_intercept=False)
+    poimodel = PoissonRegression(theta_0=[[0],[0],[0],[0],[0]])
+    PoissonRegression.fit(x_train,y_train)
+    x_valid, y_valid = util.load_dataset("../data/ds4_valid.csv",add_intercept=True)
+    pred = PoissonRegression.predict(x_valid)
+    util.plot(np.array(pred),y_valid,np.array([0,1,1]))
 
     # *** START CODE HERE ***
     # Fit a Poisson Regression model
@@ -30,6 +35,25 @@ class PoissonRegression(LinearModel):
         > clf.fit(x_train, y_train)
         > clf.predict(x_eval)
     """
+    
+    def make_list(self,x,y):
+        l = []
+        for i in range(np.size(y)):
+            poi = poisson.pmf(k=y[i],mu = np.exp(2*np.dot(self.theta.T,np.array([x[i]]).T)))
+            l.append((-1*y[i]+ np.exp((np.dot(self.theta.T,np.array([x[i]]).T))))/poi)
+        return l
+    
+    def next(self,x,y):
+        l = self.make_list(x,y)
+        addtheta = np.zeros((np.size(x)[1],1))
+        for j in range(np.size(x)[1]):
+            for i in range(np.size(x)[0]):
+                addtheta[j]+=((l[i])*x[i][j])
+        self.theta = self.theta+self.step_size*addtheta
+        
+    
+    
+        
 
     def fit(self, x, y):
         """Run gradient ascent to maximize likelihood for Poisson regression.
@@ -39,6 +63,10 @@ class PoissonRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        prevtheta = self.theta
+        count = 0
+        while count<self.max_iter:
+            next(x,y)
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -51,4 +79,9 @@ class PoissonRegression(LinearModel):
             Floating-point prediction for each input, shape (m,).
         """
         # *** START CODE HERE ***
+        pred = []
+        for i in x:
+            pred.append(np.exp(np.dot(self.theta.T,np.array([i]).T)))
+            
+        return pred
         # *** END CODE HERE ***
